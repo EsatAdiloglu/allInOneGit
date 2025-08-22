4c1a18ee26a108d99012435622701bd6198c139e
EsatAdiloglu <esatadiloglu16@gmail.com>
2025-08-22 17:04:55 -0400

    added feature so that you can push to an origin branch

--- gitAll.sh
+++ gitAll.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+O_arg=""
+
+while getopts "O:" opt; do
+    case $opt in
+        O)
+            O_arg="$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
 git add .
 git commit -m "$1"
-git push
+
+if [ -z "$O_arg" ]; then
+    git push 
+else
+    git push origin "$O_arg"
+fi
